b67623c9170bfb836fed61326ae6dde783a30d22
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-07-19 14:18:18 -0300

    fim +-

--- curso_1/index.php
+++ curso_1/index.php
@@ -1,19 +1,29 @@
 <? 
-$opcoes = [1 => 'pedra', 2 => 'papel', 3 => 'tesoura'];
+$opcoes = [0 , 1 , 2];
 
+echo "
+0_ Pedra
+1_Papel
+2_tesoura
+
+";
 echo 'Faça sua escolha: ';
 $escolha1 = trim(fgets(STDIN));
 $esc = (int)$escolha1;
 
-$escolha = 0 ;
+$eclpc = 1;
+$escolha = $opcoes[$esc];
 
 if ($escolha !== 1 and $escolha !== 2 and $escolha !== 0 ){
     echo 'Escolha errada';
 }else{
     if($escolha == $eclpc) {
         echo 'Empate';
-    } 
+    } elseif (($esc == 0 && $eclpc == 2) || ($esc == 1 && $eclpc == 0) || ($esc == 2 && $eclpc == 1)) {
+        echo 'Você venceu';
+    }else {
+        echo 'Você Perdeu';
+    }
 }
-
 ?>
 
