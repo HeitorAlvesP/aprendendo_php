41e06a3f5844a4d2048c9244e4e20274bb4ce996
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-07-19 13:56:58 -0300

    jokenpo

---
+++ curso_1/index.php
@@ -0,0 +1,19 @@
+<? 
+$opcoes = [1 => 'pedra', 2 => 'papel', 3 => 'tesoura'];
+
+echo 'Faça sua escolha: ';
+$escolha1 = trim(fgets(STDIN));
+$esc = (int)$escolha1;
+
+$escolha = 0 ;
+
+if ($escolha !== 1 and $escolha !== 2 and $escolha !== 0 ){
+    echo 'Escolha errada';
+}else{
+    if($escolha == $eclpc) {
+        echo 'Empate';
+    } 
+}
+
+?>
+
