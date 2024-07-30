a82fbe67f6bb9c3005dd3aa0606ab40e8fd93f8e
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-07-30 12:12:15 -0300

    commit

--- video_php/video_1/public/index.php
+++ video_php/video_1/public/index.php
@@ -6,8 +6,19 @@
     $usuario_logado = $_SESSION['usuario'] ?? null;
 
     if (empty($usuario_logado)){
-
+        $rota = 'login';
     }else{
-
+        $rota = $_GET['rota'] ?? 'home';
     };
+
+    $rotas = [
+        'login' => 'login.php',
+        'home' => 'home.php'
+    ];
+
+    if(!key_exists($rota, $rotas)){
+        die('Acesso negado');
+    }
+        
+    require_once $rotas[$rota];
     
---
+++ video_php/video_1/public/login.php
@@ -0,0 +1,17 @@
+<?php
+    defined('CONTROL') or die('Acesso Negado');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    
+    <form action="index.php?rota=login" method="post"></form>
+
+</body>
+</html>
