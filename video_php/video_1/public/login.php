92cd31508886cd8de32a1fa8d263b9572deef5b5
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-07-30 13:30:45 -0300

    salve

--- video_php/video_1/public/login.php
+++ video_php/video_1/public/login.php
@@ -12,6 +12,18 @@
 <body>
     
     <form action="index.php?rota=login" method="post"></form>
+    <h3>Login</h3>
+    <div>
+        <label for="usuario">Usuário</label>
+        <input type="text" name="usuario" id="usuario"  >
+    </div>
+    <div>
+        <label for="senha">Senha</label>
+        <input type="text" name="senha" id="senha"  >
+    </div>
+    <div>
+        <button type="submit">Entrar</button>
+    </div>
 
 </body>
 </html>
