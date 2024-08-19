243d852c448a19c6cd615996d1100f0204a9f958
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-08-19 11:51:20 -0300

    recomeçando php

--- alura/curso_1/screen_match.php
+++ alura/curso_1/screen_match.php
@@ -1,37 +1,17 @@
 <?php
 
-    $nome_do_filme = "Top Gun";
-    //$ano_de_lançamento = $argv[1] ?? 2024;
-    $inclui_no_plano = true;
+    echo "Bem vindo ao Screen Match \n";
 
-    $contador = $argv[1];
-    $soma_de_notas;
+    $nomeFilme = "Top Gun-Mavercik";
+    $anoDeLancamento = 2022;
+    $incluido = $planoPrime || $anoDeLancamento < 2020;
+    $planoPrime = true;
 
-    for ($contador; $contador < $argc; $contador += 1) {
-        $soma_de_notas += $argv[$contador];
-    };
+    $notasFilmes = [3, 5, 7, 8];
+    $mediaNotas = array_sum($notasFilmes) / count($notasFilmes);
+    echo "A nota é: $mediaNotas\n";
 
+    echo "Nome do Filme: $nomeFilme";
 
-    $quantidade_de_notas = $argc -1;
-    $nota_de_filme = $soma_de_notas / $quantidade_de_notas;
-
-    echo $nota_de_filme . ", é a nota do filme \n";
-
-    /*if ($ano_de_lançamento >= 2024) {
-        echo "Esse filme é um lançamento";
-    }else {
-         echo "Não é tão novo assim";
-    };*/
-
-    /*$genero = match ($nome_do_filme) {
-        "" => "Ação" ,
-        "" => "comedia",
-        "" => "Ciencia" ,
-        "" => "Romance",
-        default => ' desconhecido'
-    };*/
-
-    //echo "Esse é um filme de " . $genero ,"\n"; 
-    //echo $argc;
 
 ?>
