941e092e2e175d064e144571dba091b9db55ba8c
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-08-23 14:35:50 -0300

    function

--- alura/curso_1/screen_match.php
+++ alura/curso_1/screen_match.php
@@ -3,37 +3,44 @@
     echo "Bem vindo ao Screen Match \n";
 
     $genero = ["Ação", "Comedia", "Corrida", "Romance", "Terror"];
+    function mensagem_genero(){
+        global $filme;       
+        if($filme["genero"] == "" ){
+            echo $filme["genero"] = "Genero do filme não informado \n" ;
+        }else{
+            echo "Genero: " ,$filme["genero"], "\n";
+        };
+    }
 
     function mensagem_ano(){
         global $filme;     
         if($filme["anoDeLancamento"] >= 2024){
-            echo "Filme é uma estreia \n";
+            echo "O filme ", $filme["nomeFilme"], " é uma estreia \n";
         }elseif($filme["anoDeLancamento"] < 2024 && $filme["anoDeLancamento"] >= 2022){
-            echo "Filme é novo \n";
+            echo "O filme ", $filme["nomeFilme"], " é novo, mas não uma estreia \n";
         }else{
-            echo "Filme ne novo não \n";
+            echo "O filme ", $filme["nomeFilme"], " é um classico \n";
         };
     }
+
+    function calcular_media_nota(){
+        global $filme;
+        $mediaNotas = array_sum($filme["notasFilmes"]) / count($filme["notasFilmes"]);
+        echo "Avaliação: $mediaNotas\n";
+    };
 
 
 
     $filme = [
-        "nomeFilme" => "Filme",
+        "nomeFilme" => "Ex Nome",
         "anoDeLancamento" => 2024,
-        "genero" => $genero[1],
+        "genero" => $genero[2],
         "notasFilmes" => [6, 5, 7, 8]
     ];
 
-    if($filme["genero"] == "" ){
-        echo $filme["genero"] = "Genero do filme não informado \n" ;
-    }else{
-        echo $filme["genero"] ,"\n";
-    };
-
-     $mediaNotas = array_sum($filme["notasFilmes"]) / count($filme["notasFilmes"]);
-     echo $mediaNotas, "\n";
-
     mensagem_ano();
+    mensagem_genero();
+    calcular_media_nota();
 
 
 ?>
