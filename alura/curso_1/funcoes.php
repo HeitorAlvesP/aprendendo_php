4bf3c670fdf3dfb76f8bc67f4eebf75b1e82e46f
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-08-27 12:21:57 -0300

    Funções em arquivo separado

---
+++ alura/curso_1/funcoes.php
@@ -0,0 +1,29 @@
+<?php 
+
+    function mensagem_genero(){
+        global $filme;       
+        if($filme["genero"] == "" ){
+            echo $filme["genero"] = "Genero do filme não informado \n" ;
+        }else{
+            echo "Genero: " ,$filme["genero"], "\n";
+        };
+    }
+
+    function mensagem_ano(){
+        global $filme;     
+        if($filme["anoDeLancamento"] >= 2024){
+            echo "O filme ", $filme["nomeFilme"], " é uma estreia \n";
+        }elseif($filme["anoDeLancamento"] < 2024 && $filme["anoDeLancamento"] >= 2022){
+            echo "O filme ", $filme["nomeFilme"], " é novo, mas não uma estreia \n";
+        }else{
+            echo "O filme ", $filme["nomeFilme"], " é um classico \n";
+        };
+    }
+
+    function calcular_media_nota(){
+        global $filme;
+        $mediaNotas = array_sum($filme["notasFilmes"]) / count($filme["notasFilmes"]);
+        echo "Avaliação: $mediaNotas\n";
+    };
+
+?>
